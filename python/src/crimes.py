9768b0c907d37cf9fc5af6fffef45c62e5ac64e6
Omar Saleem <theshadowzero@gmail.com>
2014-06-06 15:05:58 -0400

    "Added crimes repo"

--- python/src/crimes.py
+++ python/src/crimes.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 import sys
 import json
 
-HEADER = {'User-Agent': 'RealTimeWeb Facebook library for educational purposes'}
+HEADER = {'User-Agent': 'RealTimeWeb crimes library for educational purposes'}
 PYTHON_3 = sys.version_info >= (3, 0)
 
 if PYTHON_3:
